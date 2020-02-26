df6451c2f631ea03f5fe31f232c4f56ba99b9427
Sam Ollason <samollason@admins-mbp.home>
2020-02-26 08:01:38 +0000

    Updates

---
+++ precache-manifest.1faa33be984ba1e4310b733d3e059d2b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ca2e542fbcdf406501e0179a919812",
+    "url": "/bristolJS-slides-feb-2020/index.html"
+  },
+  {
+    "revision": "73a6775346d731a0f15d",
+    "url": "/bristolJS-slides-feb-2020/static/css/2.4cf5f81b.chunk.css"
+  },
+  {
+    "revision": "c2e580197963033a1236",
+    "url": "/bristolJS-slides-feb-2020/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "73a6775346d731a0f15d",
+    "url": "/bristolJS-slides-feb-2020/static/js/2.782a1f23.chunk.js"
+  },
+  {
+    "revision": "c2e580197963033a1236",
+    "url": "/bristolJS-slides-feb-2020/static/js/main.aec14557.chunk.js"
+  },
+  {
+    "revision": "fff266a1ca496d28b92b",
+    "url": "/bristolJS-slides-feb-2020/static/js/runtime-main.8a87ad19.js"
+  },
+  {
+    "revision": "78a8d99862240d634dedd380e3f95bb8",
+    "url": "/bristolJS-slides-feb-2020/static/media/FlexBoxStyling.78a8d998.png"
+  },
+  {
+    "revision": "17d347ea970851d3185c1811b9a67d23",
+    "url": "/bristolJS-slides-feb-2020/static/media/InterfaceBuilderStyling1.17d347ea.png"
+  },
+  {
+    "revision": "f60f8777d821d069844e3cc7afc6dc61",
+    "url": "/bristolJS-slides-feb-2020/static/media/InterfaceBuilderStyling2.f60f8777.png"
+  },
+  {
+    "revision": "a1e5bbb91b6e280a234d320a59689ef9",
+    "url": "/bristolJS-slides-feb-2020/static/media/WebStormSShot.a1e5bbb9.png"
+  },
+  {
+    "revision": "469e71cc00a9abed92abb1c2d0689c71",
+    "url": "/bristolJS-slides-feb-2020/static/media/XcodeSShot.469e71cc.png"
+  },
+  {
+    "revision": "ebf22707bdcba61c6c88e56632963769",
+    "url": "/bristolJS-slides-feb-2020/static/media/bridge.ebf22707.png"
+  },
+  {
+    "revision": "6490d74bb1d876ee6e71eed1b46ac950",
+    "url": "/bristolJS-slides-feb-2020/static/media/firstSlide.6490d74b.png"
+  },
+  {
+    "revision": "83119921a3eac372eefc9827d913d310",
+    "url": "/bristolJS-slides-feb-2020/static/media/formidable-logo.83119921.svg"
+  },
+  {
+    "revision": "9cb168d2a6f118a435404c44d6c36ff9",
+    "url": "/bristolJS-slides-feb-2020/static/media/good-work.9cb168d2.gif"
+  },
+  {
+    "revision": "fab1b995b191a7ea2132f6c8ff4b2948",
+    "url": "/bristolJS-slides-feb-2020/static/media/jsxExample.fab1b995.png"
+  },
+  {
+    "revision": "d85be94b5c11f7d37d6f4d5626f6686b",
+    "url": "/bristolJS-slides-feb-2020/static/media/pino.d85be94b.jpg"
+  },
+  {
+    "revision": "248cb156cf25bd359ef4705fb4306762",
+    "url": "/bristolJS-slides-feb-2020/static/media/react-native-logo.248cb156.png"
+  },
+  {
+    "revision": "7e249c97cfdec6ffb742512831eef120",
+    "url": "/bristolJS-slides-feb-2020/static/media/sponsor1.7e249c97.png"
+  },
+  {
+    "revision": "92312d3dab94c1875d3cc2125ef1557a",
+    "url": "/bristolJS-slides-feb-2020/static/media/sponsor2.92312d3d.png"
+  }
+]);
